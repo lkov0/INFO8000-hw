de2616258303f06f09110d5e341c8c81ce1130b3
Lynsey <lynseykovar@gmail.com>
2020-02-13 14:17:42 -0500

    add POST and GET abilities to test_app.py

--- test_app.py
+++ test_app.py
@@ -1,22 +1,49 @@
 #test_app.py
 
 from flask import Flask, escape, request, jsonify
+from flask_api import status
 import sqlite3
 import pandas as pd
 
-conn = sqlite3.connect("sampleDB.db")
-
 app = Flask(__name__)
+key = '2f279b10631d72ec3c9e82f3e59c6d83'
 
-@app.route("/", methods = ['GET','POST'])
+@app.route("/", methods = ['GET'])
 def fun():
-    name = request.args.get("sampleid", 1)
-    cursor = conn.execute("SELECT * FROM sample WHERE sample.id = 1")
-    rows = cursor.fetchall()
-    alldata = []
-    content = {}
-    for row in rows:
-        content = {'id' = row[0], 'date' = row[1], 'sequencer_id' = row[2], 'tissue' = row[3], 'species' = row[4], 'seed_id' = row[5], 'collection_location' = row[6], 'experiment' = row[7], 'data_size' = row[8]}
-        alldata.append(content)
-        content = {}
-    return jsonify(content)
+    conn = sqlite3.connect("sampleDB.db")
+    conn.row_factory = sqlite3.Row
+    c = conn.cursor()
+    name = request.args.get("sampleid")
+    if name is not None:
+        c.execute('SELECT * FROM sample WHERE sample.id = ?', [name])
+        rows = c.fetchall()
+        results = [dict(row) for row in rows]
+        return jsonify(results)
+    else:
+        c.execute('SELECT * FROM sample')
+        rows = c.fetchall()
+        results = [dict(row) for row in rows]
+        return jsonify(results)
+
+@app.route("/", methods = ['POST'])
+def receivePost():
+    
+    clientkey = request.args.get("clientkey")
+    
+    if clientkey != key:
+        return "uh", status.HTTP_401_UNAUTHORIZED
+    else:
+        conn = sqlite3.connect("sampleDB.db")
+        conn.row_factory = sqlite3.Row
+        c = conn.cursor()
+        content = request.json
+    
+        c.execute('INSERT INTO sample VALUES (?,?,?,?,?,?,?,?,?)', (content['id'], content['date'], content['sequencer_id'], content['tissue'], content['species'], content['seed_id'], content['collection_location'], content['experiment'], content['data_size'])) 
+        conn.commit()
+    
+        c.execute('SELECT * FROM sample')
+        rows = c.fetchall()
+        results = [dict(row) for row in rows]
+        return jsonify(results)
+
+    
